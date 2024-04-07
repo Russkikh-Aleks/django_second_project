abf69dbd61c19c01017f34c44139fb0e23faa6c7
Russkikh Aleks <aleksanderrussckikh@yandex.ru>
2024-04-07 14:50:37 +0300

    Add files

---
+++ recipes/calculator/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+    # можете добавить свои рецепты ;)
+}
+
+
+def recipe(request):
+    dish = request.path[1:-1]
+    recipe = DATA[dish]
+    try:
+        num = int(request.GET.get('servings', 1))
+    except:
+        num = 1
+    
+    if num != 1:
+        for key in recipe.keys():
+            value = recipe[key]
+            if isinstance(value, float):
+                bit_depth = len(str(value).split('.')[1])
+                recipe[key] = round(value * num, bit_depth)
+            else:
+                recipe[key] = value * num
+    
+    context = {
+        'recipe': recipe
+    }
+    return render(request, 'calculator/index.html', context)
+
+
+# Напишите ваш обработчик. Используйте DATA как источник данных
+# Результат - render(request, 'calculator/index.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#   'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
